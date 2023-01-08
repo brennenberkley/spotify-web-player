5a546477455ef61a951928b2349b8a2456c16d5d
Brennen <brennenfromseattle@gmail.com>
2023-01-08 15:15:26 -0800

    token api is working

--- src/loginLambda/lambda/index.mjs
+++ src/loginLambda/lambda/index.mjs
@@ -1,15 +1,21 @@
+import Model from "./Model.mjs";
+import { fetchToken } from "./networkHelper.mjs";
+
 const CLIENT_ID = "04c1cbfe68de4f2bb0c7e016ee6c1733";
-const CLIENT_SECRET = "d43ad4e7614140f4a41c2df1afa31f15"; // TODO: remove this if publishing app;
 const REDIRECT_URI = "https://6nb5wkc5yl.execute-api.us-west-2.amazonaws.com/authorized";
 const SCOPE = "user-modify-playback-state+user-read-recently-played+user-read-playback-position+playlist-read-collaborative+app-remote-control+user-read-playback-state+streaming+user-library-modify+user-read-currently-playing+user-library-read+playlist-read-private+playlist-modify-private+user-read-email+user-read-private"
 
 const LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&scope=${SCOPE}`;
 
 export const handler = async(event) => {
+  console.log("Route key:", event.routeKey);
+
   if (event.routeKey == 'GET /login') {
     return login(event);
-  } else if (event.routeKey = 'GET /authorized') {
+  } else if (event.routeKey == 'GET /authorized') {
     return authorized(event);
+  } else if (event.routeKey == 'GET /getToken') {
+    return getToken(event);
   }
 };
 
@@ -37,7 +43,7 @@ async function authorized(event) {
 
   console.log("Received authorization code for login id:", loginId);
 
-  await _fetchInitialAccessToken(authCode);
+  await fetchToken(loginId, authCode);
 
   return {
     statusCode: 200,
@@ -45,41 +51,21 @@ async function authorized(event) {
   }
 }
 
-async function _fetchInitialAccessToken(code) {
-  const requestBody = new URLSearchParams({
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: REDIRECT_URI
-  }).toString();
+async function getToken(event) {
+  const loginId = event.queryStringParameters.id;
 
-  return await _fetchAccessToken(requestBody);
-}
-
-
-async function _fetchAccessToken(requestBody) {
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-    },
-    body: requestBody
-  };
-
-  let accessToken = null;
-
-  console.log("Starting call");
-  await window.fetch('https://accounts.spotify.com/api/token', options)
-  .then((response) => response.json())
-  .then(async (data) => {
-    accessToken = data.access_token;
-
-    await saveTokenToDatabase(data.access_token, data.refresh_token)
-  });
-}
-
-async function saveTokenToDatabase() {
-
+  let model = new Model();
+  let token = await model.getToken(loginId);
+  if (token) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(token)
+    }
+  } else {
+    return {
+      statusCode: 404
+    }
+  }
 }
 
 function buildLoginUrl(loginId) {
